00c7061632b74aa5028ef1b0242b20fa5c8cb883
ezann <rayzanmuhammadr774@gmail.com>
2025-11-04 20:44:06 +0700

    add: init login page

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./features/authentication/components/Layout";
+import UserLogin from "./features/authentication/components/UserLogin";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
+        <Route element={<Layout />}>
+          <Route path="/login" element={<UserLogin />} />
+        </Route>
+
         <Route path="/*" element={<div>Hello, World!</div>} />
       </Routes>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
