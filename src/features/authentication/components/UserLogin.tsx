3bcccd65196bc8893ad91c21e680c09d896bf2d3
ezann <rayzanmuhammadr774@gmail.com>
2025-11-05 06:32:47 +0700

    feat(auth): add login page layout and UI

--- src/features/authentication/components/UserLogin.tsx
+++ src/features/authentication/components/UserLogin.tsx
@@ -1,8 +1,58 @@
 const UserLogin = () => {
   return (
-  <div>
-    
-  </div>
+    <>
+      <div className="py-6 w-full px-6 flex flex-col gap-5">
+        <div className="flex flex-col gap-5 w-full">
+          <div className="relative">
+            <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+              <img
+                src="/src/assets/images/icons/mail.svg"
+                alt="icon"
+                className="w-5 h-5 text-[#BABABA]"
+              />
+            </span>
+
+            <input
+              type="text"
+              placeholder="Email"
+              className="w-full h-[50px] pl-10 pr-4 py-2  font-dmsans text-sm font-bold tracking-[0.2px] border text-[#BABABA] rounded-lg bg-white outline-none"
+            />
+          </div>
+          <div className="relative">
+            <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+              <img
+                src="/src/assets/images/icons/lock.svg"
+                alt="icon"
+                className="w-5 h-5 text-[#BABABA]"
+              />
+            </span>
+
+            <input
+              type="password"
+              placeholder="Password"
+              className="w-full h-[50px] pl-10 pr-4 py-2  font-dmsans text-sm font-bold tracking-[0.2px] border text-[#BABABA] rounded-lg bg-white outline-none"
+            />
+          </div>
+          <a
+            href=""
+            className="text-center font-medium text-[11px] tracking-[0.6px] font-dmsans text-[#ffffff]"
+          >
+            Forgot Password
+          </a>
+        </div>
+      </div>
+      <div className="w-full px-6 py-1 flex flex-col gap-6">
+        <button className="h-[50px] px-10 w-full bg-[#0ACF83] text-[16px] rounded-lg font-semibold font-dmsans tracking-[0.2px] text-[#ffffff]">
+          Sign In
+        </button>
+        <p className="text-center font-light text-sm font-dmsans tracking-[0.2px] text-white">
+          Didn’t have any account?{" "}
+          <a href="" className="text-[#0ACF83]">
+            Sign Up here
+          </a>
+        </p>
+      </div>
+    </>
   );
 };
 
