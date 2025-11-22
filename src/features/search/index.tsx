bc321c86062b96f031ccd0b79cb341338c660296
ezann <rayzanmuhammadr774@gmail.com>
2025-11-22 15:38:49 +0700

    feat: add search functionality for products

--- src/features/search/index.tsx
+++ src/features/search/index.tsx
@@ -1,6 +1,16 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { products } from "../products/data/products";
 
 const SearchPage = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const filteredProducts = products.filter((product) =>
+    product.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  console.log(filteredProducts);
+
   return (
     <>
       <div className="flex justify-between px-6 bg-white pt-[50px] ">
@@ -26,6 +36,8 @@ const SearchPage = () => {
 
           <input
             type="text"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
             placeholder="Search Handphone"
             className="w-full h-[45px] pl-10 pr-4 py-1 font-dmsans text-[12px] font-bold tracking-[0.2px] leading-5 border border-[#BABABA] text-[#2D2D2D] placeholder-[#BABABA] rounded-lg bg-white outline-none"
           />
@@ -63,111 +75,49 @@ const SearchPage = () => {
           Popular product
         </h1>
         <div className="flex flex-col gap-[25px]">
-          <div className="flex flex-row gap-[15px]">
-            <div className="bg-[#F6F6F6] rounded-2xl p-2.5">
-              <img
-                src="src/assets/images/pict/headphone.png"
-                className="w-20"
-                alt=""
-              />
-            </div>
-            <div className="flex flex-col items-start justify-between w-full">
-              <div className="flex flex-col gap-[5px] ">
-                <h1 className="font-dmsans font-medium text-[16px] tracking-[0.2px]">
-                  TMA-2 Comfort Wireless{" "}
-                </h1>
-                <h1 className="font-dmsans font-extrabold text-sm tracking-[0.2px] ">
-                  USD 270
-                </h1>
+          {filteredProducts.map((product) => (
+            <div key={product.id} className="flex flex-row gap-[15px] pb-10">
+              <div className="bg-[#F6F6F6] rounded-2xl p-2.5">
+                <img
+                  src={product.image}
+                  className="w-20"
+                  alt=""
+                />
               </div>
-              <div className="flex flex-row justify-between w-full items-center">
-                <div className="flex gap-2.5 ">
-                  <div className="flex gap-[3px] ">
-                    <img src="src/assets/images/icons/star-filled.svg" alt="" />
-                    <h2 className="font-dmsans text-[12px] tracking-[0.2px] font-medium">
-                      4.6
-                    </h2>
-                  </div>
-                  <span className="font-dmsans text-[12px] tracking-[0.2px] font-medium">
-                    86 Reviews
-                  </span>
+              <div className="flex flex-col items-start justify-between w-full">
+                <div className="flex flex-col gap-[5px] ">
+                  <h1 className="font-dmsans font-medium text-[16px] tracking-[0.2px]">
+                    {product.name}{" "}
+                  </h1>
+                  <h1 className="font-dmsans font-extrabold text-sm tracking-[0.2px] ">
+                    USD {product.price}
+                  </h1>
                 </div>
-                <Link to={"/search"}>
-                  <img src="src/assets/images/icons/more-vertical.svg" alt="" />
-                </Link>
-              </div>
-            </div>
-          </div>
-          <div className="flex flex-row gap-[15px]">
-            <div className="bg-[#F6F6F6] rounded-2xl p-2.5">
-              <img
-                src="src/assets/images/pict/headphone.png"
-                className="w-20"
-                alt=""
-              />
-            </div>
-            <div className="flex flex-col items-start justify-between w-full">
-              <div className="flex flex-col gap-[5px] ">
-                <h1 className="font-dmsans font-medium text-[16px] tracking-[0.2px]">
-                  TMA-2 Comfort Wireless{" "}
-                </h1>
-                <h1 className="font-dmsans font-extrabold text-sm tracking-[0.2px] ">
-                  USD 270
-                </h1>
-              </div>
-              <div className="flex flex-row justify-between w-full items-center">
-                <div className="flex gap-2.5 ">
-                  <div className="flex gap-[3px] ">
-                    <img src="src/assets/images/icons/star-filled.svg" alt="" />
-                    <h2 className="font-dmsans text-[12px] tracking-[0.2px] font-medium">
-                      4.6
-                    </h2>
+                <div className="flex flex-row justify-between w-full items-center">
+                  <div className="flex gap-2.5 ">
+                    <div className="flex gap-[3px] ">
+                      <img
+                        src="src/assets/images/icons/star-filled.svg"
+                        alt=""
+                      />
+                      <h2 className="font-dmsans text-[12px] tracking-[0.2px] font-medium">
+                        {product.rating}
+                      </h2>
+                    </div>
+                    <span className="font-dmsans text-[12px] tracking-[0.2px] font-medium">
+                      {product.reviews} Reviews
+                    </span>
                   </div>
-                  <span className="font-dmsans text-[12px] tracking-[0.2px] font-medium">
-                    86 Reviews
-                  </span>
+                  <Link to={"/search"}>
+                    <img
+                      src="src/assets/images/icons/more-vertical.svg"
+                      alt=""
+                    />
+                  </Link>
                 </div>
-                <Link to={"/search"}>
-                  <img src="src/assets/images/icons/more-vertical.svg" alt="" />
-                </Link>
               </div>
             </div>
-          </div>
-          <div className="flex flex-row gap-[15px]">
-            <div className="bg-[#F6F6F6] rounded-2xl p-2.5">
-              <img
-                src="src/assets/images/pict/headphone.png"
-                className="w-20"
-                alt=""
-              />
-            </div>
-            <div className="flex flex-col items-start justify-between w-full">
-              <div className="flex flex-col gap-[5px] ">
-                <h1 className="font-dmsans font-medium text-[16px] tracking-[0.2px]">
-                  TMA-2 Comfort Wireless{" "}
-                </h1>
-                <h1 className="font-dmsans font-extrabold text-sm tracking-[0.2px] ">
-                  USD 270
-                </h1>
-              </div>
-              <div className="flex flex-row justify-between w-full items-center">
-                <div className="flex gap-2.5 ">
-                  <div className="flex gap-[3px] ">
-                    <img src="src/assets/images/icons/star-filled.svg" alt="" />
-                    <h2 className="font-dmsans text-[12px] tracking-[0.2px] font-medium">
-                      4.6
-                    </h2>
-                  </div>
-                  <span className="font-dmsans text-[12px] tracking-[0.2px] font-medium">
-                    86 Reviews
-                  </span>
-                </div>
-                <Link to={"/search"}>
-                  <img src="src/assets/images/icons/more-vertical.svg" alt="" />
-                </Link>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </>
